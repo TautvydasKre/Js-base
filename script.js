5f56e4a2fe5169ed011ab8f61bc1e1dcd0642d78
Arnas Simaitis <113042490+ArnasSimairis@users.noreply.github.com>
2023-11-09 11:36:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,110 @@
+"use strict";
+// const tekstas = "Tautvydas kreivys ";
+// const str = "Petras petraitis";
+// const together = tekstas.concat(str);
+// console.log(together);
+
+// let text1 = "       Laba diena           ";
+// let text2 = "Kaip Sekasi ";
+// let text3 = "Ka dabar daryti";
+
+// let concatenatedText = text1.concat(text2, text3).charAt(5);
+// console.log(concatenatedText);
+
+// let concatenatedText = text1.endsWith("");
+// console.log(concatenatedText);
+
+// let concatenatedText = text1.indexOf("Laba");
+// console.log(concatenatedText);
+
+// let concatenatedText = text1.length;
+// console.log(concatenatedText);
+
+// let concatenatedText = text1.match("diena");
+// console.log(concatenatedText);
+
+// let concatenatedText = text1.repeat("3");
+// console.log(concatenatedText);
+
+// let concatenatedText = text1.replace("diena", "ryteli");
+// console.log(concatenatedText);
+
+// let concatenatedText = text1.search("na");
+// console.log(concatenatedText);
+
+// let concatenatedText = text1.slice(3, 6);
+// console.log(concatenatedText);
+
+// let concatenatedText = text1.split(" ");
+// console.log(concatenatedText);
+
+// let concatenatedText = text1.substring(5, 9);
+// console.log(concatenatedText);
+// let concatedText = text1.substring(5, 9);
+// let toUpper = text1.charAt(5).toUpperCase();
+// let subs = text1.substring(6, 10);
+// let concated = toUpper.concat(subs);
+// let text = text1.replace("diena", concated);
+// let changed = text1.replace("d", "D");
+// console.log(concatedText);
+// console.log(toUpper);
+// console.log(subs);
+// console.log(concated);
+// console.log(text);
+// console.log(changed);
+
+// let concatenatedText = text1.trim();
+// console.log(concatenatedText);
+
+// let x = 5;
+// console.log(++x);
+// let xc = 5;
+// console.log(xc++);
+// let xv = 5;
+// console.log(--xv);
+// let xb = 5;
+// console.log(xb--);
+
+let str = "Tautvydas Kreivys";
+str = "Kristupas Kolumbas";
+console.log(str);
+
+let nr = 5;
+console.log(nr);
+console.log(++nr);
+console.log(++nr);
+
+let mNr = 5;
+console.log(mNr);
+console.log(--mNr);
+console.log(--mNr);
+console.log(--mNr);
+
+let sNr = "12";
+console.log(typeof sNr);
+console.log(typeof Number(sNr));
+console.log(typeof parseInt(sNr, 10));
+console.log(typeof parseFloat(sNr));
+
+let dNr = 12;
+let sk = 10 * dNr;
+console.log(sk);
+
+let tomasWeight = 90;
+let tomasHeight = 1.77;
+let tomasKmi = tomasWeight / tomasHeight ** 2;
+console.log(tomasKmi);
+
+let tadasWeight = 40;
+let tadasHeight = 2.25;
+let tadasKmi = tadasWeight / tadasHeight ** 2;
+console.log(tadasKmi);
+
+let a = 8;
+let b = 6;
+let c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+console.log(c);
+
+let cel = 20;
+let far = (9 / 5) * cel + 32;
+console.log(far);
